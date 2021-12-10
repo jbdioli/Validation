9c101e6a82e00a2518b4e35c710ff100525d7ea7
jbdioli <jdeoworld@gmail.com>
2021-12-10 18:45:08 +0100

    Initial commit

---
+++ Validation/Validation/Helpers/Validators/IValidationRule.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Validation.Helpers.Validators
+{
+    public interface IValidationRule<T>
+    {
+        string ValidationMessage { get; set; }
+        bool Check(T value);
+    }
+}
---
+++ Validation/Validation/Views/RegisterPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Validation.ViewModels;
+using Xamarin.Forms;
+
+namespace Validation.Views
+{
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+            // BindingContext = new RegisterPageViewModel();
+        }
+    }
+}
